9e1c1be2d62c96f7aa45bd05b3e1d64f7d745462
hariprasath.krishnamoorthy@ideas2it.com <hariprasath.krishnamoorthy@ideas2it.com>
2023-04-18 16:56:09 +0530

    New Changes

--- tests/AddNewpatient.spec.ts
+++ tests/AddNewpatient.spec.ts
@@ -8,9 +8,26 @@ import {RadiographandPhoto} from "../src/pages/RadiographandPhoto";
 
 
 
-test("Verify New Patient Treatment Plan Creation and Order Confirmation ", async ({ page,request }, testResult) => {
+test("Verify New Patient Treatment Plan Creation and Order Confirmation ", async ({ request }, testResult) => {
   
-  test.setTimeout(1420000);
+  test.setTimeout(2000000);
+  const browser = await chromium.launch({
+    args: [ '--disable-site-isolation-trials', '--disable-web-security', '--allow-running-insecure-content'],
+    headless: false,
+    
+    
+  });
+
+  const context = await browser.newContext({
+    acceptDownloads: true,
+    
+   
+  });
+
+  const page = await context.newPage();
+
+
+
   await page.goto("https://qa.oemaligner.com/#/login");
 
   const login_page = new LoginPage(page);
@@ -37,8 +54,6 @@ test("Verify New Patient Treatment Plan Creation and Order Confirmation ", async
   const Scan_page = new Scans(page);
   await Scan_page.uploadUpperScan();
   await Scan_page.uploadLowerScan();
-  //await page.waitForSelector("//*[contains(text(),'Ok, got it!')]", { timeout: 5000 });
-  //await page.locator("//*[contains(text(),'Ok, got it!')]").click({force:true, delay:15000});
   await page.waitForLoadState();
   await Scan_page.moveNext();
 
@@ -47,10 +62,24 @@ test("Verify New Patient Treatment Plan Creation and Order Confirmation ", async
   await RadioandPhoto_page.moveNext();
 
 
- 
+  //treatment-plan and prep page
+  await page.waitForSelector('.MuiBackdrop-root', { timeout: 200000});
+  await expect(await page.getByText('The treatment has completed. This is a simple case.').isVisible()).toBe(true);
+  await page.getByRole('button', { name: 'View results' }).click();
 
 
-
+  //order page
+  await page.getByTestId('goToCart').press('Enter');
+  await page.waitForLoadState();
+  await page.getByTestId('consent1').getByRole('checkbox', { name: 'controlled' }).check();
+  await page.getByTestId('consent2').getByRole('checkbox', { name: 'controlled' }).check();
+  await page.getByTestId('consent3').getByRole('checkbox', { name: 'controlled' }).check();
+  await page.getByTestId('consent4').getByRole('checkbox', { name: 'controlled' }).check();
+  await page.getByRole('button', { name: 'Place order' }).click();
+  
+  await page.waitForLoadState();
+  await expect( await page.getByText('Order confirmed').isVisible()).toBe(true);
+  await page.getByRole('button', { name: 'Close' }).click();
 
 });
 
